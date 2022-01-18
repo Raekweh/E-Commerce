7a5c939d584bae4721d5714479f77da01440e542
Raekweh <Raekweh009@gmail.com>
2022-01-18 16:57:22 +1300

    Creating Home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import { Component } from 'react';
 
 import Registerpage from './pages/register-page';
+import HomePage from './pages/home-page';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Registerpage />
-      </div>
+      <Route>
+        <Route exact path ="/" component = {HomePage}/>
+        <Route path = "/register-page" component = {Registerpage}/>
+      </Route>
     );
   }
 }
---
+++ front-end/src/pages/home-page.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class HomePage extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h2>Welcome to our E-Commerce website</h2>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
